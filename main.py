f33e8d1416f400dc0909b8bb41059ee56502c3e5
Gonzalo <46397496@alumnos.frsr.utn.edu.ar>
2026-04-01 16:19:17 +0000

    Agrego Dockerfile y main.py base

---
+++ main.py
@@ -0,0 +1,8 @@
+import PyPDF2
+
+def main():
+    print("Iniciando el conversor de PDF a Texto...")
+    print("Entorno Docker y Python configurados correctamente.")
+
+if __name__ == "__main__":
+    main()
