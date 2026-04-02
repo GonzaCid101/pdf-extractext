889269459e1eb948247f384213d779fafd6e2803
Manuel <46397470@alumnos.frsr.utn.edu.ar>
2026-04-02 19:22:10 -0300

    borro main, estoy probando

--- main.py
+++
@@ -1,8 +0,0 @@
-import PyPDF2
-
-def main():
-    print("Iniciando el conversor de PDF a Texto")
-    print("Entorno Docker y Python configurados correctamente.")
-
-if __name__ == "__main__":
-    main()
