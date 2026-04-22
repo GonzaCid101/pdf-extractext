7d58353cb56a69d6aa829874e7b933de3a86b4c0
GonzaCid101 <46397496@alumnos.frsr.utn.edu.ar>
2026-04-22 14:46:30 -0300

    Correccion de nombre

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from app.api.health import router as health_router
 from app.api.endpoints.upload import router as upload_router
 
-app = FastAPI(title="Proyecto Cabras - API Conversor PDF")
+app = FastAPI(title="Proyecto Convertidor - API Conversor PDF")
 
 app.include_router(health_router)
 app.include_router(upload_router)
